6e5455491f9e3d925776d22d5248df0443bb65de
kebibibi <iisakki.ritovuori@edu.hel.fi>
2023-10-12 16:53:39 +0300

    u

--- Assets/Bucket.cs
+++ Assets/Bucket.cs
@@ -10,6 +10,7 @@ public class Bucket : MonoBehaviour
     public Transform player;
     public GameObject fish;
     public GameObject door;
+    public GameObject hook;
 
     private void Start()
     {
@@ -19,16 +20,26 @@ public class Bucket : MonoBehaviour
     void Update()
     {
         Vector3 playerDir = transform.position - player.position;
+        Vector3 hookToPlayer = player.position - hook.transform.position;
+        Vector3 bucketHook = new Vector3(-18.3f, 6.8f, -935.75f);
 
-        if(fishCount >= 5 && playerDir.magnitude < dist)
+        if(fishCount == 5 && playerDir.magnitude < dist)
         {
-            gameObject.SetActive(false);
             Vector3 fishSpawn = new Vector3(45, 6, -992);
+            Vector3 fishSpawn1 = new Vector3(46, 3.59f, -934);
+
+            transform.position = new Vector3(0, 0, 0);
 
             Instantiate(fish, fishSpawn, transform.rotation);
+            Instantiate(fish, fishSpawn, transform.rotation);
+            Instantiate(fish, fishSpawn1, transform.rotation);
+            Instantiate(fish, fishSpawn1, transform.rotation);  
+            Instantiate(fish, fishSpawn1, transform.rotation);
         }
-        if(fishCount >= 10)
+
+        if(fishCount >= 10 && hookToPlayer.magnitude > 9)
         {
+            transform.position = bucketHook;
             door.SetActive(true);
         }
     }
