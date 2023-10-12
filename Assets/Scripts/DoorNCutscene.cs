72afd05745f7831a0d4b4bd6a02ec4e8aa5a3c77
kebibibi <iisakki.ritovuori@edu.hel.fi>
2023-10-12 17:16:52 +0300

    uninin

---
+++ Assets/Scripts/DoorNCutscene.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorNCutscene : MonoBehaviour
+{
+    public Transform player;
+    public GameObject endScene;
+
+    void Update()
+    {
+        Vector3 winDis = transform.position - player.position;
+
+        if(winDis.magnitude < 3)
+        {
+            endScene.SetActive(true);
+        }
+    }
+}
