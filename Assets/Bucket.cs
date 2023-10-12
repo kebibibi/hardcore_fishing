625549c2b34ce9cca58481aea475944bfbd6d49a
kebibibi <iisakki.ritovuori@edu.hel.fi>
2023-10-12 15:59:51 +0300

    uni

--- Assets/Bucket.cs
+++ Assets/Bucket.cs
@@ -8,6 +8,13 @@ public class Bucket : MonoBehaviour
     public float dist;
 
     public Transform player;
+    public GameObject fish;
+    public GameObject door;
+
+    private void Start()
+    {
+        door.SetActive(false);
+    }
 
     void Update()
     {
@@ -16,6 +23,13 @@ public class Bucket : MonoBehaviour
         if(fishCount >= 5 && playerDir.magnitude < dist)
         {
             gameObject.SetActive(false);
+            Vector3 fishSpawn = new Vector3(45, 6, -992);
+
+            Instantiate(fish, fishSpawn, transform.rotation);
+        }
+        if(fishCount >= 10)
+        {
+            door.SetActive(true);
         }
     }
 }
